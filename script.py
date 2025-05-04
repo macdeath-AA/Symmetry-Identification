552cc2454331e0b7d4ff00d05c025fe715f81df1
aditi <me20btech11001@iith.ac.in>
2025-05-04 11:44:24 -0400

    get face properties

--- script.py
+++ script.py
@@ -4,32 +4,51 @@ from OCC.Core.TopExp import TopExp_Explorer
 from OCC.Core.TopAbs import TopAbs_FACE
 from OCC.Core.BRepGProp import brepgprop_SurfaceProperties
 from OCC.Core.GProp import GProp_GProps
+from OCC.Core.BRep import BRep_Tool
+from OCC.Core.BRepTools import breptools_UVBounds
+from OCC.Core.gp import gp_Pnt, gp_Vec
+import numpy as np
 
-reader = STEPControl_Reader()
-status = reader.ReadFile("GearShaft.step")
-reader.TransferRoots()
-shape = reader.OneShape()
+def read_step_file(filename):
+    reader = STEPControl_Reader()
+    reader.ReadFile(filename)
+    reader.TransferRoots()
+    return reader.OneShape()
 
-display,start_display,_,_ = init_display()
-display.DisplayShape(shape, update=True)
+def get_face_properties(shape):
+    explorer = TopExp_Explorer(shape, TopAbs_FACE)
+    face_properties = []
 
-face_explorer = TopExp_Explorer(shape, TopAbs_FACE)
-face_centers = []
-
-while face_explorer.More():
-    face = face_explorer.Current()
-    props = GProp_GProps()
-    brepgprop_SurfaceProperties(face, props)
+    while explorer.More():
+        face = explorer.Current()
+        props = GProp_GProps()
+        brepgprop_SurfaceProperties(face, props)
+        center = props.CentreOfMass()
+        area = props.Mass()
+        umin, umax, vmin, vmax = breptools_UVBounds(face)
+        normal_vec = gp_Vec()
+        BRep_Tool.Surface(face).D1((umin+ umax)/2, (vmin+vmax)/2, gp_Pnt(), normal_vec, gp_Vec())
+        face_properties.append({
+            'center': (center.X(), center.Y(), center.Z()),
+            'area': area,
+            'normal': (normal_vec.X(), normal_vec.Y(), normal_vec.Z())
+        })
+        explorer.Next()
     
-    # Get the center of the face
-    center = props.CentreOfMass()
-    face_centers.append((center.X(), center.Y(), center.Z()))
-    face_explorer.Next()
+    return face_properties
 
-print("Face centers:")
-for i, c in enumerate(face_centers):
-    print(f"Face {i + 1}: Center = {c}")
+if __name__ == "__main__":
+    filename= "GearShaft.step"
+    shape = read_step_file(filename)
 
-start_display()
+    # Get face properties
+    face_data = get_face_properties(shape)
+    print("Total Faces Found:", len(face_data))
 
+    for i, face in enumerate(face_data):
+        print(f"Face {i+1}: Center = {face['center']}, Area = {face['area']:.4f}, Normal = {face['normal']}")
 
+    #Visualization
+    display, start_display, _,_ = init_display()
+    display.DisplayShape(shape, update=True)
+    start_display()
