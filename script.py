a2d383a8644d68519cd9e1df6cdd3fd0d065435f
aditi <me20btech11001@iith.ac.in>
2025-05-04 14:58:48 -0400

    Best mirror plane

--- script.py
+++ script.py
@@ -9,6 +9,7 @@ from OCC.Core.BRepBuilderAPI import BRepBuilderAPI_MakeFace
 from OCC.Core.BRepTools import breptools_UVBounds
 from OCC.Core.gp import gp_Pnt, gp_Vec, gp_Pln, gp_Dir
 from OCC.Core.Geom import Geom_Plane
+from scipy.spatial import cKDTree
 import numpy as np
 import random
 
@@ -87,13 +88,66 @@ def visualize_candidate_planes(display, candidate_planes,count=1):
         plane_face = BRepBuilderAPI_MakeFace(plane_geom, -size, size, -size, size).Face()
         display.DisplayShape(plane_face, update=True, color='BLUE1', transparency=0.5)
 
-def visualize_random_candidate_planes(display, candidate_planes, count=5):
-    random_planes = random.sample(candidate_planes,count)
-    visualize_candidate_planes(display, random_planes, count= count)
+def reflect_point(point, plane_point, plane_normal):
+    v = np.array(point) - plane_point
+    return plane_point + v -2 * np.dot(v,plane_normal) * plane_normal
+
+def reflect_vector(vec, plane_normal):
+    return vec - 2 * np.dot(vec, plane_normal) * plane_normal
+
+def pairwise_matching_score(face_data, plane):
+    pts = np.array([f['center'] for f in face_data])
+    norms = np.array([f['normal'] for f in face_data])
+    areas = np.array([f['area'] for f in face_data])
+    tree = cKDTree(pts)
+
+    p0 = np.array(plane['midpoint'])
+    N = np.array(plane['plane_normal'])
+
+    dist_error = 0.0
+    norm_error = 0.0
+    area_error = 0.0
+    M = len(face_data)
+
+    for i in range(M):
+        c,n,a = pts[i], norms[i], areas[i]
+
+        #reflect center --> find nearest real face
+        c_ref = reflect_point(c, p0, N)
+        d,j = tree.query(c_ref)
+        dist_error += d 
+
+        #reflect normal --> compare alignment
+        n_ref = reflect_vector(n, N)
+        n_ref /= np.linalg.norm(n_ref) 
+        n_j = norms[j]/ np.linalg.norm(norms[j])
+        norm_error += (1 - abs(np.dot(n_ref, n_j)))
+
+        #compare areas
+        area_error += abs(a - areas[j]) / max(a, areas[j])
+    
+    return {
+        'avg_dist_error': dist_error / M,
+        'avg_norm_error': norm_error / M,
+        'avg_area_error': area_error / M
+    }
+
+def find_best_plane(face_data, candidate_planes):
+    best, best_score = None, None
+
+    for plane in candidate_planes:
+        errs = pairwise_matching_score(face_data, plane)
+
+        total_err = errs['avg_dist_error'] + errs['avg_norm_error'] + errs['avg_area_error']
+
+        if best is None or total_err < best_score:
+            best_score, best = total_err, plane
+    
+    return best
 
 
 if __name__ == "__main__":
-    filename= "JAWSLIDING.step"
+    filename= "GearShaft.step"
     shape = read_step_file(filename)
 
     # Get face properties
@@ -103,9 +157,15 @@ if __name__ == "__main__":
     #candidate planes
     candidate_planes_list = candidate_planes(face_data)
     print("Total Candidate Planes Found:", len(candidate_planes_list))
+
+    #best plane
+    best_plane = find_best_plane(face_data, candidate_planes_list)
+    print("Best Plane Found:", best_plane)
     
     #Visualization
     display, start_display, _, _ = init_display()
     display.DisplayShape(shape, update=True)
+
+    visualize_candidate_planes(display, [best_plane], count=11)
 
     start_display()
