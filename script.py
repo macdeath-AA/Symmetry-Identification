1772a17708fc78f290ecd2674edce8a42389b7a4
aditi <me20btech11001@iith.ac.in>
2025-05-04 15:08:15 -0400

    update function variables

--- script.py
+++ script.py
@@ -13,13 +13,13 @@ from scipy.spatial import cKDTree
 import numpy as np
 import random
 
-def read_step_file(filename):
+def readStepFile(filename):
     reader = STEPControl_Reader()
     reader.ReadFile(filename)
     reader.TransferRoots()
     return reader.OneShape()
 
-def get_face_properties(shape):
+def getFaceProperties(shape):
     explorer = TopExp_Explorer(shape, TopAbs_FACE)
     face_properties = []
 
@@ -42,7 +42,7 @@ def get_face_properties(shape):
     
     return face_properties
 
-def vector_between(p1,p2):
+def vectorBetween(p1,p2):
     return (p2[0] - p1[0], p2[1] - p1[1], p2[2] - p1[2])
 
 def normalize(vec):
@@ -54,7 +54,7 @@ def normalize(vec):
 def midpoint(p1, p2):
     return ((p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2, (p1[2] + p2[2]) / 2)
 
-def candidate_planes(face_properties):
+def candidatePlanes(face_properties):
     candidate_planes = []
 
     for i in range(len(face_properties)):
@@ -65,7 +65,7 @@ def candidate_planes(face_properties):
             center1 = f1['center']
             center2 = f2['center']
             mid = midpoint(center1, center2)
-            nvec = normalize(vector_between(center1, center2))
+            nvec = normalize(vectorBetween(center1, center2))
 
             plane = {
                 'face1_index': i,
@@ -78,7 +78,7 @@ def candidate_planes(face_properties):
     
     return candidate_planes
 
-def visualize_candidate_planes(display, candidate_planes,count=1):
+def visualizeCandidatePlanes(display, candidate_planes,count=1):
     
     for i, plane_info in enumerate(candidate_planes[:count]):
         origin = gp_Pnt(*plane_info['midpoint'])
@@ -88,14 +88,14 @@ def visualize_candidate_planes(display, candidate_planes,count=1):
         plane_face = BRepBuilderAPI_MakeFace(plane_geom, -size, size, -size, size).Face()
         display.DisplayShape(plane_face, update=True, color='BLUE1', transparency=0.5)
 
-def reflect_point(point, plane_point, plane_normal):
+def reflectPoint(point, plane_point, plane_normal):
     v = np.array(point) - plane_point
     return plane_point + v -2 * np.dot(v,plane_normal) * plane_normal
 
-def reflect_vector(vec, plane_normal):
+def reflectVector(vec, plane_normal):
     return vec - 2 * np.dot(vec, plane_normal) * plane_normal
 
-def pairwise_matching_score(face_data, plane):
+def PairwiseMatchingScore(face_data, plane):
     pts = np.array([f['center'] for f in face_data])
     norms = np.array([f['normal'] for f in face_data])
     areas = np.array([f['area'] for f in face_data])
@@ -113,12 +113,12 @@ def pairwise_matching_score(face_data, plane):
         c,n,a = pts[i], norms[i], areas[i]
 
         #reflect center --> find nearest real face
-        c_ref = reflect_point(c, p0, N)
+        c_ref = reflectPoint(c, p0, N)
         d,j = tree.query(c_ref)
         dist_error += d 
 
         #reflect normal --> compare alignment
-        n_ref = reflect_vector(n, N)
+        n_ref = reflectVector(n, N)
         n_ref /= np.linalg.norm(n_ref) 
         n_j = norms[j]/ np.linalg.norm(norms[j])
         norm_error += (1 - abs(np.dot(n_ref, n_j)))
@@ -132,11 +132,11 @@ def pairwise_matching_score(face_data, plane):
         'avg_area_error': area_error / M
     }
 
-def find_best_plane(face_data, candidate_planes):
+def findBestPlane(face_data, candidate_planes):
     best, best_score = None, None
 
     for plane in candidate_planes:
-        errs = pairwise_matching_score(face_data, plane)
+        errs = PairwiseMatchingScore(face_data, plane)
 
         total_err = errs['avg_dist_error'] + errs['avg_norm_error'] + errs['avg_area_error']
 
@@ -148,24 +148,24 @@ def find_best_plane(face_data, candidate_planes):
 
 if __name__ == "__main__":
     filename= "GearShaft.step"
-    shape = read_step_file(filename)
+    shape = readStepFile(filename)
 
     # Get face properties
-    face_data = get_face_properties(shape)
+    face_data = getFaceProperties(shape)
     print("Total Faces Found:", len(face_data))    
 
     #candidate planes
-    candidate_planes_list = candidate_planes(face_data)
+    candidate_planes_list = candidatePlanes(face_data)
     print("Total Candidate Planes Found:", len(candidate_planes_list))
 
     #best plane
-    best_plane = find_best_plane(face_data, candidate_planes_list)
+    best_plane = findBestPlane(face_data, candidate_planes_list)
     print("Best Plane Found:", best_plane)
     
     #Visualization
     display, start_display, _, _ = init_display()
     display.DisplayShape(shape, update=True)
 
-    visualize_candidate_planes(display, [best_plane], count=11)
+    visualizeCandidatePlanes(display, [best_plane], count=11)
 
     start_display()
